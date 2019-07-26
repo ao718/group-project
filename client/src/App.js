18594be3815967568362625dfc68249e86c30fbd
Ryan Long <freestyleman24@gmail.com>
2019-07-26 10:39:48 -0600

    ryans first push

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React from "react"
-import {Switch,Route} from "react-router-dom"
-// import Home from "./components/Home.js"
+// import {Switch,Route} from "react-router-dom"
+import Home from "./components/Home.js"
 // import Footer from "./components/Footer.js"
 
 const App = props => {
     return (
         <div>
-
+            <Home/>    
         </div>
-        // <Switch>
-        //     <Route exact path="/" component={Home} />
+        
+    //     <Switch>
+    //         <Route exact path="/" component={Home} />
 
-        // </Switch>
-        // <Footer/>
-    )
-}
+    //     </Switch>
+    //     <Footer/>
+     )
+    }
 
 export default App
