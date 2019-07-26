cef066441b3e07158513d76afe1f33ff13aeed0a
Aaron Sanderson <ronsanderson09@gmail.com>
2019-07-26 10:55:55 -0600

    a lot of changes, context

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = props => {
+    return(
+        <div className="home">
+            this is the home page 
+        </div>
+    )
+}
+
+export default Home
