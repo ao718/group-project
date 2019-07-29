233b67ce7d0883c0515842c78f1695b7c18c601e
Aaron Sanderson <ronsanderson09@gmail.com>
2019-07-29 11:31:23 -0600

    fixed request

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Switch,Route} from "react-router-dom"
 import Home from "./components/Home.js"
-import Footer from "./components/Footer.js"
+// import Footer from "./components/Footer.js"
 import Men from "./components/Men.js"
 import Header from "./components/Header.js"
 import Results from "./components/Results.js"
@@ -15,7 +15,7 @@ const App = props => {
             <Route path="/men" render = {routerProps => <Men {...routerProps} /> } />
             <Route path="/results" render = {routerProps => <Results {...routerProps} />} />
         </Switch>
-       <Footer/>
+       {/* <Footer/> */}
         </>
     )
 }
---
+++ client/src/components/BrowseButton.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BrowseButton = props => {
+    return(
+    <button className = "browseButton">
+        press to browse & filter
+    </button>
+    )
+}
+
+export default BrowseButton
