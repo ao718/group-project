d4c3567ca5fe3851daea6c81a1cf28cf8be0ac78
Ryan Long <freestyleman24@gmail.com>
2019-07-30 14:20:00 -0600

    pushing this shit

--- client/src/components/Men.js
+++ client/src/components/Men.js
@@ -1,22 +1,25 @@
 import React from "react"
 import {withInventory} from "../context/InventoryProvider.js"
+import ItemCard from './ItemCard.js'
+import Results from "./Results.js";
 
 class Men extends React.Component{
     constructor(props){
         console.log(props)
-        super()
+        super(props)
         this.state={
 
         }
     }
     componentDidMount(){
-        this.props.getByBrand("Yeezy")
+        this.props.getByBrand("")
     }
     render(){
     return(
     <div className = "men">
         This is the men page 
-        {this.props.inventory.map(item => <h1 key={item._id}>{item.brand}</h1> )}
+        
+        {this.props.inventory.map(item => <ItemCard key={Results._id} brand={item.brand} image={item.imgUrl} price={item.price}/> )}
     </div>
     )
     }
